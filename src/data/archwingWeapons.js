a75600fdc2a1e4c4b1b04a41878a4eef65f10701
Alex Gabites <alex@netspeed.net.nz>
2018-02-10 21:53:30 +1300

    Change mastery ranks as per update 22.12.0.
    Refactored files so they are in alphabetical order, should help with changes in future.
    Added masteryRank key to every item.
    Fixed a bunch of instances of mastery_Rank that were on melee weapons.
    Fixed a bunch of incorrect mastery ranks.

--- src/data/archwingWeapons.js
+++ src/data/archwingWeapons.js
@@ -1,31 +1,105 @@
 const constants = require('../constants.js');
 
 const {
-  MARKET, TENNO_LAB, CHEM_LAB, FOMORIAN_SABOTAGE, BARO_KITEER,
+  BARO_KITEER,
+  CHEM_LAB,
+  FOMORIAN_SABOTAGE,
+  MARKET,
+  TENNO_LAB,
 } = constants.acquisition;
 
-const { THE_ARCHWING } = constants.acquisitionQuests;
+const {
+  THE_ARCHWING,
+} = constants.acquisitionQuests;
 
 module.exports = {
   archgun: [
-    { name: 'Corvas', acquisition: MARKET },
-    { name: 'Cyngas', acquisition: MARKET, masteryRank: 4 },
-    { name: 'Dual Decurion', acquisition: MARKET },
-    { name: 'Fluctus', acquisition: TENNO_LAB },
-    { name: 'Grattler', acquisition: CHEM_LAB },
-    { name: 'Imperator', acquisition: THE_ARCHWING },
-    { name: 'Imperator Vandal', acquisition: FOMORIAN_SABOTAGE },
-    { name: 'Phaedra', acquisition: MARKET },
-    { name: 'Velocitus', acquisition: TENNO_LAB },
+    {
+      name: 'Corvas',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Cyngas',
+      acquisition: MARKET,
+      masteryRank: 4,
+    },
+    {
+      name: 'Dual Decurion',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Fluctus',
+      acquisition: TENNO_LAB,
+      masteryRank: 0,
+    },
+    {
+      name: 'Grattler',
+      acquisition: CHEM_LAB,
+      masteryRank: 0,
+    },
+    {
+      name: 'Imperator',
+      acquisition: THE_ARCHWING,
+      masteryRank: 0,
+    },
+    {
+      name: 'Imperator Vandal',
+      acquisition: FOMORIAN_SABOTAGE,
+      masteryRank: 0,
+    },
+    {
+      name: 'Phaedra',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Velocitus',
+      acquisition: TENNO_LAB,
+      masteryRank: 0,
+    },
   ],
   archmelee: [
-    { name: 'Agkuza', acquisition: MARKET },
-    { name: 'Centaur', acquisition: MARKET },
-    { name: 'Kaszas', acquisition: MARKET },
-    { name: 'Knux', acquisition: CHEM_LAB },
-    { name: 'Onorix', acquisition: MARKET },
-    { name: 'Rathbone', acquisition: MARKET },
-    { name: 'Veritux', acquisition: THE_ARCHWING },
-    { name: 'Prisma Veritux', acquisition: BARO_KITEER },
+    {
+      name: 'Agkuza',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Centaur',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Kaszas',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Knux',
+      acquisition: CHEM_LAB,
+      masteryRank: 0,
+    },
+    {
+      name: 'Onorix',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Rathbone',
+      acquisition: MARKET,
+      masteryRank: 0,
+    },
+    {
+      name: 'Veritux',
+      acquisition: THE_ARCHWING,
+      masteryRank: 0,
+    },
+    {
+      name: 'Prisma Veritux',
+      acquisition: BARO_KITEER,
+      masteryRank: 0,
+    },
   ],
 };
--- src/data/sentinelWeapons.js
+++ src/data/sentinelWeapons.js
@@ -1,16 +1,64 @@
 module.exports = {
   sentinelWeapons: [
-    { name: 'Artax', acquisition: 'Taxon Sentinel' },
-    { name: 'Burst Laser', acquisition: 'Shade Sentinel' },
-    { name: 'Prisma Burst Laser', acquisition: 'Prisma Shade Sentinel' },
-    { name: 'Deconstructor', acquisition: 'Helios Sentinel' },
-    { name: 'Deconstructor Prime', acquisition: 'Helios Prime Sentinel', masteryRank: 8 },
-    { name: 'Deth Machine Rifle', acquisition: 'Dethcube Sentinel' },
-    { name: 'Laser Rifle', acquisition: 'Wyrm Sentinel' },
-    { name: 'Prime Laser Rifle', acquisition: 'Wyrm Prime Sentinel' },
-    { name: 'Stinger', acquisition: 'Djinn Sentinel' },
-    { name: 'Sweeper', acquisition: 'Carrier Sentinel' },
-    { name: 'Sweeper Prime', acquisition: 'Carrier Prime Sentinel' },
-    { name: 'Vulklok', acquisition: 'Diriga Sentinel' },
+    {
+      name: 'Artax',
+      acquisition: 'Taxon Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Burst Laser',
+      acquisition: 'Shade Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Prisma Burst Laser',
+      acquisition: 'Prisma Shade Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Deconstructor',
+      acquisition: 'Helios Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Deconstructor Prime',
+      acquisition: 'Helios Prime Sentinel',
+      masteryRank: 8,
+    },
+    {
+      name: 'Deth Machine Rifle',
+      acquisition: 'Dethcube Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Laser Rifle',
+      acquisition: 'Wyrm Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Prime Laser Rifle',
+      acquisition: 'Wyrm Prime Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Stinger',
+      acquisition: 'Djinn Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Sweeper',
+      acquisition: 'Carrier Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Sweeper Prime',
+      acquisition: 'Carrier Prime Sentinel',
+      masteryRank: 0,
+    },
+    {
+      name: 'Vulklok',
+      acquisition: 'Diriga Sentinel',
+      masteryRank: 0,
+    },
   ],
 };
