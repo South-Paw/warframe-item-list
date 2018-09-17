420367005235035f1626431863c3e4413a6fee9e
Alex Gabites <alex@netspeed.net.nz>
2018-09-17 22:32:00 +1200

    Lua CI test

--- .travis/setenv_lua.sh
+++ .travis/setenv_lua.sh
@@ -1,4 +1,4 @@
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 bash .travis/setup_lua.sh
 eval `$HOME/.lua/luarocks path`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
