7fa4302647c737df8dddafa35b63218665ec6093
Alex Gabites <alex@netspeed.net.nz>
2018-09-17 22:39:09 +1200

    Reverting lua changes

--- .travis/setenv_lua.sh
+++
@@ -1,4 +0,0 @@
-export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-bash .travis/setup_lua.sh
-eval `$HOME/.lua/luarocks path`
