c2e6b364c69e87595c574253464e8132cdb2874b
Alex Gabites <alex@netspeed.net.nz>
2018-10-13 17:39:40 +1300

    Run updater

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const constants = require('./constants.js');
 const objects = require('./objects.js');
 
 module.exports = {
-  version: '23.9.1',
+  version: '23.10.2',
   array,
   objects,
   constants,
